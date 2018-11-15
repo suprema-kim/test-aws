7baff707e9588db1aa674a86b31d54f3010a20ae
ALam, Kim <alkim@suprema.co.kr>
2018-11-15 14:22:43 +0900

    Sample code for jwt.

--- src/main/java/com/example/jwt/jwtsample/JwtsampleApplication.java
+++ src/main/java/com/example/jwt/jwtsample/JwtsampleApplication.java
@@ -1,12 +1,30 @@
 package com.example.jwt.jwtsample;
 
+import com.example.jwt.jwtsample.properties.CustomConfigProperties;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
-public class JwtsampleApplication {
+public class JwtsampleApplication implements CommandLineRunner {
+    @Autowired
+    private CustomConfigProperties customConfigProperties;
 
     public static void main(String[] args) {
         SpringApplication.run(JwtsampleApplication.class, args);
+    }
+
+    @Override
+    public void run(String... args) throws Exception {
+        System.out.println("custom.age: " + customConfigProperties.getAge());
+        System.out.println("custom.company.name: " + customConfigProperties.getCompany().getName());
+    }
+
+    @Bean
+    public RestTemplate restTemplate() {
+        return new RestTemplate();
     }
 }
--- src/main/java/com/example/jwt/jwtsample/jwt/JwtTestController.java
+++ src/main/java/com/example/jwt/jwtsample/jwt/JwtTestController.java
@@ -1,24 +1,32 @@
 package com.example.jwt.jwtsample.jwt;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
 
 import java.util.HashMap;
 import java.util.Map;
 
+@Slf4j
 @RestController
 public class JwtTestController {
     private static final String key = "abcde";
 
     @Autowired
-    JwtTokenBuilder jwtTokenBuilder;
+    private RestTemplate restTemplate;
+
+    @Autowired
+    private JwtTokenBuilder jwtTokenBuilder;
 
     @RequestMapping(value = "/token", method = RequestMethod.GET)
     public ResponseEntity<String> getToken() {
+        log.info("/token called...");
+
         Map<String, Object> claims = new HashMap<>();
         claims.put("iat", System.currentTimeMillis());
         claims.put("sub", "test");
@@ -29,6 +37,16 @@ public class JwtTestController {
 
     @RequestMapping(value = "/echo")
     public ResponseEntity<String> echo() {
+        log.info("/echo called...");
+
         return new ResponseEntity<>("yahoo", HttpStatus.OK);
+    }
+
+    @RequestMapping(value = "/call")
+    public ResponseEntity<String> callOther() {
+        log.info("AAAAAA : call server2");
+        String baseUrl = "http://172.31.18.21";
+        String res = restTemplate.getForObject(baseUrl + ":9000/call", String.class);
+        return new ResponseEntity<>(res, HttpStatus.OK);
     }
 }
