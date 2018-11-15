b76d9ce9e4e0dfc50b33b4b949ce6e1a98327eb4
ALam, Kim <alkim@suprema.co.kr>
2018-11-15 14:23:01 +0900

    Test for custom properties.

---
+++ src/main/java/com/example/jwt/jwtsample/properties/CustomConfigProperties.java
@@ -0,0 +1,59 @@
+package com.example.jwt.jwtsample.properties;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+
+@Configuration
+@PropertySource("classpath:custom.properties")
+@ConfigurationProperties(prefix = "custom")
+public class CustomConfigProperties {
+    private int age;
+    private String name;
+    private Company company;
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Company getCompany() {
+        return company;
+    }
+
+    public void setCompany(Company company) {
+        this.company = company;
+    }
+
+    public static class Company {
+        private String name;
+        private String year;
+
+        public String getName() {
+            return name;
+        }
+
+        public void setName(String name) {
+            this.name = name;
+        }
+
+        public String getYear() {
+            return year;
+        }
+
+        public void setYear(String year) {
+            this.year = year;
+        }
+    }
+}
